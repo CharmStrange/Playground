6c694c020964fcee0805e7e1835d1474267f45d9
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-04-30 20:15:10 +0900

    Create kalman_2024v.lua

---
+++ Interested/kalman_2024v.lua
@@ -0,0 +1,8 @@
+-- SYSTEM MODEL 
+-- is the most important thing in Kalman Filter.
+-- set state variables, noises of SYSTEM MODEL
+
+-- set initial posteriori state
+-- set initial covariance matrix
+
+-- implementing Recursion
